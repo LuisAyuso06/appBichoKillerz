eec9eaa27c970b4ba5f3eb95c7f8831402d8e850
ChiXDD <danychi2003@gmail.com>
2024-10-25 20:04:41 -0600

    AddProductForm

--- src/AddProductForm.tsx
+++ src/AddProductForm.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useCartContext } from './CartContext';
+import { useCartContext } from './contexts/CartContext';
 
 const productSchema = z.object({
   name: z.string().min(1, 'El nombre es requerido'),
