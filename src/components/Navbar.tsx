c9e407a224407ac7c23c8a62f055ae76ed6de156
Luis Ayuso <152822420+LuisAyuso06@users.noreply.github.com>
2024-12-01 15:32:10 -0600

    prueba vercel

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import SearchBar from './SearchBar';
+// import SearchBar from './SearchBar';
 
 const Navbar: React.FC = () => {
 
